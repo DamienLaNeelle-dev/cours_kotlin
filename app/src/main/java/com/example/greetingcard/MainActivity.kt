386fd12b3551bb33b9f4cd23226a6e91bd21590c
Damien L <damien.laneelle33@gmail.com>
2025-03-19 16:04:20 +0100

    learning in progress

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.greetingcard
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,11 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -22,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
@@ -47,6 +51,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Display(){
+    val showText = remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -56,10 +62,11 @@ fun Display(){
         CustomButton(
             name = "Test",
             onClick = {
-                println("test")
+                showText.value = !showText.value
             }
         )
-        NewText("Blablabla test d'un nouveau texte")
+        NewText("Blablabla test d'un nouveau texte", showText = showText.value)
+        ButtonPrompt()
     }
 }
 
@@ -101,16 +108,60 @@ fun CustomButton(name: String, modifier: Modifier = Modifier, onClick: () -> Uni
 }
 
 @Composable
-fun NewText(text: String, modifier: Modifier = Modifier){
-    Text(
-        text = text
+fun NewText(text: String, modifier: Modifier = Modifier, showText: Boolean){
+    if (showText) {
+        Text(
+            text = text,
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun ButtonPrompt(){
+
+    val userInput = remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        EditTextInput(value = userInput.value, onValueChange = { userInput.value = it })
+    }
+    Button(
+        onClick = {
+            Toast.makeText(
+                LocalContext.current,
+                "Test prompt: ${userInput.value}",
+                Toast.LENGTH_SHORT
+            ).show()
+        },
+        modifier = Modifier.padding(top = 16.dp)
+    ) {
+        Text(text = "Soumettre")
+    }
+}
+
+@Composable
+fun EditTextInput(value: String, onValueChange: (String) -> Unit){
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text("Entrez quelque chose...") },
+        modifier = Modifier.padding(16.dp)
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreetingCardTheme {
         Greeting("Damien", "Who's Next?")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserInputPreview() {
+    GreetingCardTheme {
+        ButtonPrompt()
     }
 }
